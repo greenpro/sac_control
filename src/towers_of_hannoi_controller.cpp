d93ace8d3bb6048a32b746eb905dfbe3b92cbaf4
Craig Christensen <crg.chrstnsn@gmail.com>
2017-04-05 18:05:56 -0500

    Addition of a new gripper which works.

--- src/towers_of_hannoi_controller.cpp
+++ src/towers_of_hannoi_controller.cpp
@@ -7,6 +7,7 @@
 #include <geometry_msgs/Twist.h>
 #include <sac_msgs/Target.h>
 //#include <sac_msgs/Path.h>
+#include <sac_msgs/HandPos.h>
 
 #define CONTROLLER_NUM (1)
 #define PI (3.1459265)
@@ -21,35 +22,70 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
 
     ros::Publisher targets = nh.advertise<sac_msgs::Target>("/moveto", 1000);
-    sac_msgs::Target msg;
+    sac_msgs::Target targetMsg;
+    ros::Publisher hand = nh.advertise<sac_msgs::HandPos>("/handDriver", 1000);
+    sac_msgs::HandPos handMsg;
 
     sleep(10);
     while (1)
     {
-        msg.x = 0.300;
-        msg.y = -0.040;
-        msg.z = 0.000;
-        msg.pitch = PI / 2;
-        msg.roll = 0;
-        targets.publish(msg);
+        targetMsg.x = 0.300;
+        targetMsg.y = -0.040;
+        targetMsg.z = 0.000;
+        targetMsg.pitch = PI / 2;
+        targetMsg.roll = 0;
+        targets.publish(targetMsg);
+        
+        handMsg.width = 0.000;
+        hand.publish(handMsg);
 
         sleep(5);
 
-        msg.x = 0.400;
-        msg.y = 0.000;
-        msg.z = 0.100;
-        msg.pitch = 0;
-        msg.roll = 0;
-        targets.publish(msg);
+        targetMsg.x = 0.400;
+        targetMsg.y = 0.000;
+        targetMsg.z = 0.100;
+        targetMsg.pitch = 0;
+        targetMsg.roll = PI / 2;
+        targets.publish(targetMsg);
+        
+        handMsg.width = 0.065;
+        hand.publish(handMsg);
 
         sleep(5);
 
-        msg.x = 0.387;
-        msg.y = 0.000;
-        msg.z = 0.570;
-        msg.pitch = 0;
-        msg.roll = 0;
-        targets.publish(msg);
+        targetMsg.x = 0.387;
+        targetMsg.y = 0.000;
+        targetMsg.z = 0.570;
+        targetMsg.pitch = 0;
+        targetMsg.roll = 0;
+        targets.publish(targetMsg);
+        
+        handMsg.width = 0.032;
+        hand.publish(handMsg);
+
+        sleep(5);
+
+        targetMsg.x = 0.387;
+        targetMsg.y = 0.000;
+        targetMsg.z = 0.570;
+        targetMsg.pitch = 0;
+        targetMsg.roll = PI / 2;
+        targets.publish(targetMsg);
+        
+        handMsg.width = 0.000;
+        hand.publish(handMsg);
+
+        sleep(5);
+
+        targetMsg.x = 0.387;
+        targetMsg.y = 0.000;
+        targetMsg.z = 0.570;
+        targetMsg.pitch = 0;
+        targetMsg.roll = -PI / 2;
+        targets.publish(targetMsg);
+        
+        handMsg.width = 0.64;
+        hand.publish(handMsg);
 
         sleep(5);
     }
