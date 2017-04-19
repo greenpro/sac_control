5a6e66a8a7831610db9680395bb158b6c68e9337
Craig Christensen <crg.chrstnsn@gmail.com>
2017-04-19 14:20:20 -0500

    The system can now withstand a disconnected robot and the robot disconnecting in the middle of the run.

--- src/towers_of_hannoi_controller.cpp
+++ src/towers_of_hannoi_controller.cpp
@@ -33,20 +33,27 @@ int main(int argc, char **argv)
     ros::init(argc, argv, towers::nodeName);
 
     ros::NodeHandle nh;
+    ROS_INFO("-------------------------started");
 
     ros::Publisher targets = nh.advertise<sac_msgs::Target>("/moveto", 1000);
     sac_msgs::Target targetMsg;
     ros::Publisher hand = nh.advertise<sac_msgs::HandPos>("/handDriver", 1000);
     sac_msgs::HandPos handMsg;
+    ROS_INFO("-------------------------publishers");
 
+    sleep(40);
+    ROS_INFO("-------------------------sleep finish");
     // planning scene
     moveit::planning_interface::PlanningSceneInterface planningSceneInterface;
+    ROS_INFO("-------------------------planning scene");
     // planning group
     moveit::planning_interface::MoveGroupInterface moveGroup(towers::planningGroup);
+    ROS_INFO("-------------------------planning group");
     // collision object
     moveit_msgs::CollisionObject collisionObject;
     collisionObject.header.frame_id = moveGroup.getPlanningFrame();
     collisionObject.id = "towerBase";
+    ROS_INFO("-------------------------collision");
     // define the object
     shape_msgs::SolidPrimitive primitive;
     primitive.type = primitive.BOX;
@@ -54,12 +61,14 @@ int main(int argc, char **argv)
     primitive.dimensions[0] = 0.4;
     primitive.dimensions[1] = 0.4;
     primitive.dimensions[2] = 0.4;
+    ROS_INFO("-------------------------object definition");
     // define the object pos
     geometry_msgs::Pose boxPose;
     boxPose.orientation.w = 1.0;
     boxPose.position.x = 0.6;
     boxPose.position.y = -0.4;
     boxPose.position.z = 1.2;
+    ROS_INFO("-------------------------object pose");
 
     collisionObject.primitives.push_back(primitive);
     collisionObject.primitive_poses.push_back(boxPose);
@@ -67,11 +76,12 @@ int main(int argc, char **argv)
 
     std::vector<moveit_msgs::CollisionObject> collisionObjects;
     collisionObjects.push_back(collisionObject);
+    ROS_INFO("-------------------------collision object");
 
     // Add the objects to the world
     planningSceneInterface.addCollisionObjects(collisionObjects);
+    ROS_INFO("-------------------------added");
 
-    sleep(40);
     while (towers::enabled)
     {
         targetMsg.x = 0.336;
